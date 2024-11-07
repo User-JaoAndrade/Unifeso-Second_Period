45364b9ae7e587fa273df2acb5c9b85a1fdc12d9
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-07 19:31:49 -0300

    Trocando pra lógica original de Kaprekar

--- AV2/questao_02.py
+++ AV2/questao_02.py
@@ -1,39 +1,49 @@
 """Passo a passo pra saber se um número é de kaprekar
     - Eleve o numero ao quadrado
-    - Divida o resultado em duas partes por exemplo 3025 = 30, 25
+    - Divida o resultado em duas partes
+    ps: a parte da direita precisa ter a mesma quantidade de digitos que o numero original
+        e a parte esquerda o resto dos digitos
+        por exemplo: 297 -> 297^2 -> 88209 -> 88 + 209
     - Some as duas partes
     - Verifique se a soma é igual ao numero original
 """
 import time
-import os
-
-def limpando_terminal()-> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 while True:
-    print(">> NÚMERO DE KAPREKAR <<")
     while True:
+        print(">> NÚMERO DE KAPREKAR <<")
         try:
-            numero: int = int(input("\nInforme um número de ate 2 digitos ->  "))
-            str_numero = str(numero) # Convertendo o número para uma string
+            numero: int = int(input("\nInforme um número de até 2 dígitos ->  "))
+
+            # Verificando se o número é valido
+            if numero < 1 or numero > 100000000:
+                print("Número inválido, informe um número válido")
+                time.sleep(3)
+                continue
+
+            # Convertendo o número para uma string
+            str_numero = str(numero)
             break
+
         except ValueError:
-            print("informe um número inteiro")
-            time.sleep(3)
+            print("Informe um número inteiro.")
+            time.sleep(1)
+
+    numero_ao_quadrado: int = numero ** 2 # Elevando o número original ao quadrado
+    str_numero_ao_quadrado = str(numero_ao_quadrado)
+
+    # Dividindo o número ao quadrado em duas partes
+    parte_direita = str_numero_ao_quadrado[-len(str_numero):]  # Últimos 'tamanho_num_original' dígitos
+    parte_esquerda = str_numero_ao_quadrado[:-len(str_numero)] or "0"  # O restante (se não houver, coloca 0)
     
-    # Dividindo a string em 2 partes
+    if parte_esquerda == '':
+        parte_esquerda = '0'
 
-    # MEXER NESSA LOGICA DEPOIS, SE EU COLOCAR 88209 E USAR A PRIMEIRA DIVISÃO ELE RETORNA 1,
-    # MAS SE USAR A SEGUNDA ELE RETORNA 0
-    ponto_de_divisao: int = len(str_numero) // 2 # A primeira metade é menor
-    ponto_de_divisao: int = (len(str_numero)+1) // 2 # A primeira metade é maior
+    # Calculando a soma das duas partes
+    num_kaprekar = int(parte_esquerda) + int(parte_direita)
 
-    primeira_metade_num: str = str_numero[:ponto_de_divisao] 
-    segunda_metade_num: str = str_numero[ponto_de_divisao:]
-
-    num_kaprekar: int = (int(primeira_metade_num) + int(segunda_metade_num)) **2
-
+    # Verificando se o número é de Kaprekar
     if num_kaprekar == numero:
         print(1)
-    else: 
+    else:
         print(0)
