0cfe5081b23f66a58366f41675e9e67584ce39a3
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-10 11:02:18 -0300

    Adicionando lógica de mostrar as letras que mais apareceram

--- AV2/questao_03.py
+++ AV2/questao_03.py
@@ -10,13 +10,24 @@ import time
 import os
 from collections import Counter
 
-def limpando_terminal()-> None:
+def limpando_terminal() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
 
 # Função que verifica as letras mais repetidas
 def calculando_letras() -> None:
-    contador_de_letras = Counter(cadeia_de_caracteres)
-    ...
+    contando_letras = Counter(cadeia_de_caracteres) # Contando as letras
+    as_mais_brabas = sorted(contando_letras.most_common(2)) # as duas que mais apareceram
+
+    # Caso as letras tenham aparecido a mesma quantidade de
+    if as_mais_brabas[0][1] == as_mais_brabas[1][1]:
+        as_mais_brabas = sorted(as_mais_brabas) # Deixando em ordem alfabética
+        for letra, contagem in as_mais_brabas:
+            print(f"{letra}: {contagem}")
+            break
+    else:
+        # Pegando as duas letras mais comuns em ordem decrescente
+        for letra, contagem in as_mais_brabas:
+            print(f"{letra}: {contagem}")
 
 if __name__ == "__main__":
     while True:
@@ -25,10 +36,11 @@ if __name__ == "__main__":
         
         # Verificando se a cadeia de letras possui <= 1000 caracteres e se tem espaços
         if len(cadeia_de_caracteres) > 1000 or ' ' in cadeia_de_caracteres:
-            print("\nPor favor, informe um lista com:\n"
+            print("\nPor favor, informe uma lista com:\n"
                   "- No Máximo 1000 caracteres\n" 
                   "- Sem espaços")
             time.sleep(5)
             continue
 
         calculando_letras()
+        break
