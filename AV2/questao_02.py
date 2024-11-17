35ea3157b87a1b585095e09067fd56b352e807a9
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-17 11:05:08 -0300

    Agora o programa verifica todas as possibilidades de soma ao dividir o número

--- AV2/questao_02.py
+++ AV2/questao_02.py
@@ -10,40 +10,39 @@
 import time
 
 while True:
-    while True:
-        print(">> NÚMERO DE KAPREKAR <<")
-        try:
-            numero: int = int(input("\nInforme um número de até 2 dígitos ->  "))
+    print(">> NÚMERO DE KAPREKAR <<")
+    try:
+        numero: int = int(input("\nInforme um número de até 2 dígitos ->  "))
 
-            # Verificando se o número é valido
-            if numero < 1 or numero > 100000000:
-                print("Número inválido, informe um número válido")
-                time.sleep(3)
-                continue
+        # Verificando se o número é valido
+        if numero < 1 or numero > 100000000:
+            print("Número inválido, informe um número válido")
+            time.sleep(3)
+            continue
 
-            # Convertendo o número para uma string
-            str_numero = str(numero)
-            break
+        # Convertendo o número para uma string
+        str_numero = str(numero)
+        break
 
-        except ValueError:
-            print("Informe um número inteiro.")
-            time.sleep(1)
+    except ValueError:
+        print("Informe um número inteiro.")
+        time.sleep(1)
 
-    numero_ao_quadrado: int = numero ** 2 # Elevando o número original ao quadrado
-    str_numero_ao_quadrado = str(numero_ao_quadrado)
+string_numero: str = str(numero) # número convertido pra uma string
+diminuir_string: int = len(string_numero) - 1
 
-    # Dividindo o número ao quadrado em duas partes
-    parte_direita = str_numero_ao_quadrado[-len(str_numero):]  # Últimos 'tamanho_num_original' dígitos
-    parte_esquerda = str_numero_ao_quadrado[:-len(str_numero)] or "0"  # O restante (se não houver, coloca 0)
-    
-    if parte_esquerda == '':
-        parte_esquerda = '0'
+while diminuir_string > 0:
+    esquerda_string: str = string_numero[:-diminuir_string] # parte esquerda
+    direita_string: str = string_numero[-diminuir_string:] # parte direita
 
-    # Calculando a soma das duas partes
-    num_kaprekar = int(parte_esquerda) + int(parte_direita)
+    kaprekar: int = (int(esquerda_string) + int(direita_string)) ** 2 # Calculo de Kaprekar
 
-    # Verificando se o número é de Kaprekar
-    if num_kaprekar == numero:
+    # Verificando se é um número de kaprekar
+    if kaprekar == numero:
         print(1)
+        exit()
     else:
-        print(0)
+        diminuir_string-=1 # decrementa em 1 o tamanho da string
+
+print(0)
+exit()
