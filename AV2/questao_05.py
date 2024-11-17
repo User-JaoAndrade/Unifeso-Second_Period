a61cf5a043b52f088329e52020fffc3872c3acee
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-17 11:01:42 -0300

    Adicionando um data frame na questão_05

--- AV2/questao_05.py
+++ AV2/questao_05.py
@@ -3,4 +3,11 @@ from pandas import DataFrame
 import datetime
 
 
-...
+if __name__ == "__main__":
+
+    # Carregando o CSV
+    data_frame = pd.read_csv('atendimentos.csv')
+    
+    # Convertendo colunas para o tipo datetime
+    data_frame['inicio'] = pd.to_datetime(data_frame['inicio']) 
+    data_frame['fim'] = pd.to_datetime(data_frame['fim'])
