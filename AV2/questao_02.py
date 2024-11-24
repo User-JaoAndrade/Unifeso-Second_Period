12d6a779bf6fce4490b846c91f7cce0fcb4eb9be
User-JaoAndrade <user.name_jaum@outlook.com>
2024-11-24 09:23:25 -0300

    Mudança insignificante

--- AV2/questao_02.py
+++ AV2/questao_02.py
@@ -9,40 +9,41 @@
 """
 import time
 
-while True:
-    print(">> NÚMERO DE KAPREKAR <<")
-    try:
-        numero: int = int(input("\nInforme um número de até 2 dígitos ->  "))
+if __name__ == "__main__":
+    while True:
+        print(">> NÚMERO DE KAPREKAR <<")
+        try:
+            numero: int = int(input("\nInforme um número de até 2 dígitos ->  "))
 
-        # Verificando se o número é valido
-        if numero < 1 or numero > 100000000:
-            print("Número inválido, informe um número válido")
-            time.sleep(3)
-            continue
+            # Verificando se o número é valido
+            if numero < 1 or numero > 100000000:
+                print("Número inválido, informe um número válido")
+                time.sleep(3)
+                continue
 
-        # Convertendo o número para uma string
-        str_numero = str(numero)
-        break
+            # Convertendo o número para uma string
+            str_numero = str(numero)
+            break
 
-    except ValueError:
-        print("Informe um número inteiro.")
-        time.sleep(1)
+        except ValueError:
+            print("Informe um número inteiro.")
+            time.sleep(1)
 
-string_numero: str = str(numero) # número convertido pra uma string
-diminuir_string: int = len(string_numero) - 1
+    string_numero: str = str(numero) # número convertido pra uma string
+    diminuir_string: int = len(string_numero) - 1
 
-while diminuir_string > 0:
-    esquerda_string: str = string_numero[:-diminuir_string] # parte esquerda
-    direita_string: str = string_numero[-diminuir_string:] # parte direita
+    while diminuir_string > 0:
+        esquerda_string: str = string_numero[:-diminuir_string] # parte esquerda
+        direita_string: str = string_numero[-diminuir_string:] # parte direita
 
-    kaprekar: int = (int(esquerda_string) + int(direita_string)) ** 2 # Calculo de Kaprekar
+        kaprekar: int = (int(esquerda_string) + int(direita_string)) ** 2 # Calculo de Kaprekar
 
-    # Verificando se é um número de kaprekar
-    if kaprekar == numero:
-        print(1)
-        exit()
-    else:
-        diminuir_string-=1 # decrementa em 1 o tamanho da string
+        # Verificando se é um número de kaprekar
+        if kaprekar == numero:
+            print(1)
+            exit()
+        else:
+            diminuir_string-=1 # decrementa em 1 o tamanho da string
 
-print(0)
-exit()
+    print(0)
+    exit()
